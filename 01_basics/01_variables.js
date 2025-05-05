5ef1666fa4d9ba80b859e0a1b3266e0bb14aa6c1
Vaibhaw Wadhwa <vaibhawwadhwa05@gmail.com>
2025-05-05 20:34:05 +0000

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "vaibhaw@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+//accountId = 2 //Not allowed
+
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use VAR, because of issues in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello"); //we are using node.js not browser
+
+console.log(3+3); console.log("Vaibhaw") 
+/*semicolon can introduce new line, but not preffered for code readability*/
+
+let name = "Vaibhaw"
+let age = 34
+let isLoggedIn = false
+
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => decalred but not assigned variable
+// symbol => unique(mostly used in React)
+
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
